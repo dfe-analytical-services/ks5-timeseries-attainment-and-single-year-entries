e107f8591e7c90e98772cb05fac0451893d57a81
Pat Thomas <patience.thomas@education.gov.uk>
2023-03-28 11:05:14 +0100

    Update beta banner

--- ui.R
+++ ui.R
@@ -89,7 +89,10 @@ ui <- function(input, output, session) {
         "in the last 12 months and we'd really like to know what you think of them. ",
         "If you're interested in helping us improve our products, please sign up ",
         "using our <a href='https://forms.office.com/e/ZjNxf10uuN'>user-testing volunteer form</a>.</b><br>",
-        "This Dashboard is in beta phase and we are still reviewing performance and reliability. "
+        "This Dashboard is in beta phase and we are still reviewing performance and reliability. ",
+        "In case of slowdown or connection issues due to high demand, we have produced two instances of this site which can be accessed at the following links: ",
+        "<a href=", site_primary, " id='link_site_1'>Site 1</a> and ",
+        "<a href=", site_overflow, " id='link_site_2'>Site 2</a>."
       )
     ),
     shiny::navlistPanel(
