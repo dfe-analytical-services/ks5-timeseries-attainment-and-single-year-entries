bf6d40bb80700a29f1ec9828ae515356eb5b6794
Rich Bielby <richard.bielby@education.gov.uk>
2023-11-14 18:28:23 +0000

    Added expandable function to dfe resources script

--- R/dfe_resources.R
+++ R/dfe_resources.R
@@ -6,3 +6,17 @@
 # (the two commented out hex codes are for the suggested colours for two 
 # additional series if absolutely necessary).
 gss_colour_pallette <- c('#12436D', '#F46A25', '#801650','#28A197') #,'#3D3D3D','#A285D1')
+
+expandable <- function(inputId, label, contents) {
+  govDetails <- shiny::tags$details(
+    class = "govuk-details", id = inputId,
+    shiny::tags$summary(
+      class = "govuk-details__summary",
+      shiny::tags$span(
+        class = "govuk-details__summary-text",
+        label
+      )
+    ),
+    shiny::tags$div(contents)
+  )
+}
